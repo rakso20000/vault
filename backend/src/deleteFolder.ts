6c8309cabb142436817c7bf91138013a38c3cee9
David Wolff <s0578072@htw-berlin.de>
2022-07-14 10:54:15 +0200

    Delete files when folder is deleted

--- backend/src/deleteFolder.ts
+++ backend/src/deleteFolder.ts
@@ -1,5 +1,6 @@
 import {addEndpoint} from './endpoints';
 import {db} from './db';
+import {LargeObjectManager} from 'pg-large-object';
 
 type Args = {
 	cipherFolderName: string;
@@ -9,12 +10,29 @@ addEndpoint<Args>('deleteFolder', 'DELETE', {
 	cipherFolderName: 'base64'
 }, async ({cipherFolderName}) => {
 	
-	await db.none(`
-		DELETE FROM folders
-		WHERE
-			cipher_name = $1;
-	`, [
-		cipherFolderName
-	]);
+	await db.tx(async tx => {
+		
+		const files = await tx.manyOrNone<{oid: number}>(`
+			SELECT oid FROM files WHERE
+				folder = (SELECT id FROM folders WHERE
+					cipher_name = $1);
+		`, [
+			cipherFolderName
+		]);
+		
+		console.log(files);
+		
+		const largeObjectManager = new LargeObjectManager({pgPromise: tx});
+		
+		await Promise.all(files.map(file => largeObjectManager.unlinkAsync(file.oid)));
+		
+		await tx.none(`
+			DELETE FROM folders WHERE
+					cipher_name = $1;
+		`, [
+			cipherFolderName
+		]);
+		
+	});
 	
 });
